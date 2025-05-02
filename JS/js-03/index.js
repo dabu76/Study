1581d14e45cfc30eb8d4a8f7a5a133701913d817
dabu76 <153888541+dabu76@users.noreply.github.com>
2025-05-02 21:53:40 +0900

    Add files via upload
    Signed-off-by: dabu76 <153888541+dabu76@users.noreply.github.com>

--- JS/js-03/index.js
+++ JS/js-03/index.js
@@ -21,6 +21,12 @@ function check() {
     alert("PWとIDを入力してください");
 
     return false;
+  } else if (!/\S+@\S+\.\S+/.test(id2)) {
+    alert("メールの形式ではありません");
+    return false;
+  } else if (!/[A-Z]/.test(pw2)) {
+    alert("PWに問題があります");
+    return false;
   } else if (pw2.length < 6) {
     alert("PWは7分以上です");
     return false;
@@ -51,3 +57,86 @@ function on_off(count) {
     $(".badge").html("Dark 🔄");
   }
 }
+
+t_count = 4;
+let timer = setInterval(() => {
+  time();
+}, 1000);
+function time() {
+  if (t_count >= 0) {
+    $("#p_sec").html(t_count);
+    t_count--;
+  } else {
+    $(".alert").hide();
+    clearInterval(timer);
+  }
+}
+// let now_page = 1;
+// $(".next").on("click", () => {
+//   now_page++;
+//   if (now_page === 1) {
+//     $(".slide-container").removeClass("slide-animation slide-animation2");
+//   } else if (now_page === 2) {
+//     $(".slide-container")
+//       .removeClass("slide-animation2")
+//       .addClass("slide-animation");
+//   } else if (now_page === 3) {
+//     $(".slide-container")
+//       .removeClass("slide-animation")
+//       .addClass("slide-animation2");
+//   } else if (now_page > 3) {
+//     $(".slide-container").removeClass("slide-animation slide-animation2");
+//     now_page = 1;
+//   }
+// });
+// $(".before").on("click", () => {
+//   now_page--;
+//   if (now_page === 1) {
+//     $(".slide-container").removeClass("slide-animation slide-animation2");
+//   } else if (now_page === 2) {
+//     $(".slide-container")
+//       .removeClass("slide-animation2")
+//       .addClass("slide-animation");
+//   } else if (now_page < 1) {
+//     $(".slide-container").addClass("slide-animation2");
+//     now_page = 3;
+//   }
+// });
+const totalSlides = $(".slide-box").length;
+let total_width = totalSlides * 100 + "vw";
+let p_count = 0;
+$(".slide-container").css("width", total_width);
+$(".next").on("click", () => {
+  p_count++;
+  if (p_count < totalSlides) {
+    now_page = p_count * 100 + "vw";
+    $(".slide-container").css("transform", `translateX(-${now_page})`);
+  } else if (p_count <= totalSlides) {
+    p_count = 0;
+    $(".slide-container").css("transform", "translateX(0vw)");
+  }
+});
+$(".before").on("click", () => {
+  p_count--;
+  if (p_count >= 0) {
+    let now_page = p_count * 100 + "vw";
+    $(".slide-container").css("transform", `translateX(-${now_page})`);
+  } else if (p_count < 0) {
+    p_count = totalSlides - 1;
+    let now_page = p_count * 100 + "vw";
+    $(".slide-container").css("transform", `translateX(-${now_page})`);
+  }
+});
+$(".slide-1").on("click", () => {
+  $(".slide-container").removeClass("slide-animation slide-animation2");
+});
+$(".slide-2").on("click", () => {
+  $(".slide-container")
+    .removeClass("slide-animation2")
+    .addClass("slide-animation");
+});
+$(".slide-3").on("click", () => {
+  $(".slide-container")
+    .removeClass("slide-animation")
+    .addClass("slide-animation2");
+});
