615e65dfb2b4a5469fc4706fde0d1d00643d5400
dabu76 <153888541+dabu76@users.noreply.github.com>
2025-05-03 20:55:30 +0900

    Add files via upload
    Signed-off-by: dabu76 <153888541+dabu76@users.noreply.github.com>

---
+++ JS/JS-04/index.js
@@ -0,0 +1,34 @@
+// let b_length = $(".tab-button").length;
+// for (let i = 0; i < b_length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       tab_open(i);
+//     });
+// }
+$(".list").on("click", function (e) {
+  tab_open(e.target.dataset.id);
+});
+function tab_open(i) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(i).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(i).addClass("show");
+}
+
+// $(".tab-button")
+//   .eq(1)
+//   .on("click", function () {
+//     $(".tab-button").removeClass("orange");
+//     $(".tab-button").eq(1).addClass("orange");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-content").eq(1).addClass("show");
+//   });
+// $(".tab-button")
+//   .eq(2)
+//   .on("click", function () {
+//     $(".tab-button").removeClass("orange");
+//     $(".tab-button").eq(2).addClass("orange");
+//     $(".tab-content").removeClass("show");
+//     $(".tab-content").eq(2).addClass("show");
+//   });
--- JS/js-03/index.js
+++ JS/js-03/index.js
@@ -13,6 +13,15 @@ $("#login").on("click", function () {
 $("#close").on("click", function () {
   $(".black-bg").removeClass("show-modal");
 });
+document.querySelector(".black-bg").addEventListener("click", function (e) {
+  // e.target; //ユーザーが実際押したもの
+  // e.currentTarget; //eventlistenerがあるところ
+  // e.preventDefault(); //基本動作を防止
+  // e.stopPropagation(); //上位要素でのイベントバブルリンクを防ぐ
+  if (e.target == document.querySelector(".black-bg")) {
+    $(".black-bg").removeClass("show-modal");
+  }
+});
 function check() {
   const id2 = document.getElementById("id3").value;
   const pw2 = document.getElementById("pw3").value;
@@ -116,6 +125,7 @@ $(".next").on("click", () => {
     $(".slide-container").css("transform", "translateX(0vw)");
   }
 });
+
 $(".before").on("click", () => {
   p_count--;
   if (p_count >= 0) {
@@ -139,4 +149,26 @@ $(".slide-3").on("click", () => {
   $(".slide-container")
     .removeClass("slide-animation")
     .addClass("slide-animation2");
+});
+window.addEventListener("scroll", function () {
+  let scroll_height = window.scrollY;
+  let change_font = 100;
+  console.log(scroll_height);
+  console.log(change_font);
+  if (scroll_height >= change_font) {
+    $(".navbar-brand").css("font-size", "20px");
+  }
+  if (scroll_height < change_font) {
+    $(".navbar-brand").css("font-size", "30px");
+  }
+});
+
+$(".lorem").on("scroll", function () {
+  const scroll = document.querySelector(".lorem").scrollTop;
+  const real_scroll = document.querySelector(".lorem").scrollHeight;
+  const height = document.querySelector(".lorem").clientHeight;
+  const real_height = scroll + height;
+  if (real_scroll <= real_height) {
+    alert("同意しますか");
+  }
 });
